bb8b49b9df5fc1f187f01e44a611ce4e77723085
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-05-08 09:37:55 +0530

    12

---
+++ sending reset password link/frontend/login/login.js
@@ -0,0 +1,13 @@
+function submitForm(event) {
+    event.preventDefault();
+    const form=new FormData(event.target)
+    const loginDetails={
+        email:form.get('email'),
+        password:form.get('password')
+    }
+    axios.post(`http://localhost:3000/login`,loginDetails).then((response) => {
+        localStorage.setItem(`JWTTOKEN`,JSON.stringify(response.data.token))
+        localStorage.setItem(`USER_DETAILS`,JSON.stringify(response.data.user))
+        window.location.href = "../expense/expense.html";
+    }).catch((error) => {console.log(error);})
+}
